843571d058181b2597ad0ebcf813511f12b8b917
Rodolfo <rluthen@gmail.com>
2023-11-23 22:16:26 -0600

    Switch eval to PeSTO

--- src/evaluation.py
+++ src/evaluation.py
@@ -5,13 +5,23 @@ from psqt import *
 import chess
 
 
+gamephase_inc = {
+    chess.PAWN: 0,
+    chess.KNIGHT: 1,
+    chess.BISHOP: 1,
+    chess.ROOK: 2,
+    chess.QUEEN: 4,
+    chess.KING: 0,
+}
+
+
 class Evaluation:
     @staticmethod
     def eval_side(board: chess.Board, color: chess.Color) -> int:
         occupied = board.occupied_co[color]
 
-        material = 0
-        psqt = 0
+        eg, mg = 0, 0
+        game_phase = 0
 
         # loop over all set bits
         while occupied:
@@ -19,21 +29,28 @@ class Evaluation:
             square = lsb(occupied)
 
             piece = board.piece_type_at(square)
+            if piece == None:
+                continue
 
-            # add material
-            material += piece_values[piece]
+            # add piece values
+            if color == chess.WHITE:
+                eg += eg_psqt_values[piece][square]  # type: ignore
+                mg += mg_psqt_values[piece][square]  # type: ignore
+            else:
+                eg += list(reversed(eg_psqt_values[piece]))[square]  # type: ignore
+                mg += list(reversed(mg_psqt_values[piece]))[square]  # type: ignore
 
-            # add piece square table value
-            psqt += (
-                list(reversed(psqt_values[piece]))[square]
-                if color == chess.BLACK
-                else psqt_values[piece][square]
-            )
+            game_phase += gamephase_inc[piece]  # type: ignore
 
             # remove lsb
             occupied = poplsb(occupied)
 
-        return material + psqt
+        # get phase factors
+        mg_phase = min(game_phase, 24)
+        eg_phase = 24 - mg_phase
+
+        # return scores with phase factors
+        return int((mg * mg_phase + eg * eg_phase) / 24)
 
     @staticmethod
     def evaluate(board: chess.Board) -> int:
